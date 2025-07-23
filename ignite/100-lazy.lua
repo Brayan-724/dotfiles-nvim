81278dc58b5289389a3f4b9764d7c75a31facbef
Brayan-724 <bjqf0724@gmail.com>
2025-07-22 20:45:01 -0700

    fix(ignite/lazy): Bad installation of lazy

--- ignite/100-lazy.lua
+++ ignite/100-lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.cmd "redraw"
   vim.api.nvim_echo({ { "  Installing lazy.nvim & plugins ...", "Bold" } }, true, {})
 
-  local output = vim.fn.system { "git", "clone", "--depth=1", "--filter=blob:none", "--branch=stable", repo, lazypath }
+  local output = vim.fn.system { "git", "clone", "--filter=blob:none", "--branch=stable", repo, lazypath }
   assert(vim.v.shell_error == 0, "External call failed with error code: " .. vim.v.shell_error .. "\n" .. output)
 
 end
