c3cf12b715655f39d25e8bfff61c34ca55108edd
Brayan-724 <bjqf0724@gmail.com>
2026-01-13 18:12:08 -0700

    refactor: remove unused files

--- lua/apika/plugins/base/devicons.lua
+++
@@ -1,8 +0,0 @@
-------- DEFINITION -------
-return {
-  "nvim-tree/nvim-web-devicons",
-  opts = require("apika.config.devicons").opts,
-  config = function(_, opts)
-    require("nvim-web-devicons").setup(opts)
-  end,
-}
--- lua/apika/plugins/base/mini.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "echasnovski/mini.base16",
-    version = "*",
-    config = function()
-      vim.cmd.colorscheme "apika"
-    end,
-  },
-}
